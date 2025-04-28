9a11a1511e0202ea927e42ee4cd3887e5c0c6cb2
92username <115041664+92username@users.noreply.github.com>
2025-04-28 18:37:06 -0300

    feat: Add JavaScript examples

---
+++ conceitos-sintaxe/ifElse.js
@@ -0,0 +1,33 @@
+// pseudo codigo
+// Estrutura de decisão e operadores lógicos.
+if (condição1 && condição2){ // E
+    instrução1;
+    instrução2;
+    }
+    if (condição1 || condição2){ // ou
+        instrução1;
+        instrução2;
+        }
+
+        if (!condição1){ // negação
+            instrução1;
+            instrução2;
+            }
+
+// Exemplo de uso de if e else / pseudo código
+            if(número fornecido é inteiro e positivo){
+                Guarde o número em uma variável;
+           }else{
+                Avise ao usuário que o número não é válido;
+                Solicite ao usuário que insira novamente um número;
+           }
+
+           if (numero1 < 0){
+            instrução1;
+            }else if(numero == 0){
+                 instrução2;
+            }else{
+                 instrução3;
+            }
+            
+            
